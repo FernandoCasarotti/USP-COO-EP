006e719f84861f9354e7b318bc0393c8cc825451
leogiraldimg <leogiraldimg@usp.br>
2018-04-12 23:25:44 -0300

    v1

--- PhaustoFire_Transportes/src/beans/Cliente.java
+++ PhaustoFire_Transportes/src/beans/Cliente.java
@@ -1,67 +1,21 @@
 package beans;
 
-public class Cliente{
-    private String nome;
+public class Cliente extends Pessoa{
     private String cnpj;
-    private String endereco;
-    private String complemento;
-    private String cep;
-    private String estado;
-    private String cidade;
-
-    public Cliente(String nome, String cnpj, String endereco, 
-        String complemento, String cep, String estado, String cidade){
-            this.setNome(nome);
-            this.setCnpj(cnpj);
-            this.setEndereco(endereco);
-            this.setComplemento(complemento);
-            this.setCep(cep);
-            this.setEstado(estado);
-            this.setCep(cidade);
-    }
-
-	public String getNome() {
-		return nome;
+	
+	public Cliente(String nome, String endereco, String complemento, String cep, 
+	String estado, String cidade, String cnpj){
+		super(nome, endereco, complemento, cep, estado, cidade);
+		this.setCnpj(cnpj);
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+
 	public String getCnpj() {
 		return cnpj;
 	}
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	public String getComplemento() {
-		return complemento;
-	}
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-	public String getCep() {
-		return cep;
-	}
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-	public String getEstado() {
-		return estado;
-	}
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-	public String getCidade() {
-		return cidade;
-	}
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
+	
 
     
 }
---
+++ PhaustoFire_Transportes/src/beans/Funcionario.java
@@ -0,0 +1,7 @@
+package beans;
+
+public class Funcionario extends Pessoa{
+    private String cpf;
+    private String cargo;
+    
+}
